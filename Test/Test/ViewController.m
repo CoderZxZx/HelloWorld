947ba863c5f4d7a04a9a8d5a5b36629af56f484f
张晓 <807424525@qq.com>
2015-12-06 11:47:41 +0800

    second changed

--- Test/Test/ViewController.m
+++ Test/Test/ViewController.m
@@ -20,6 +20,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"helloTest");
+    NSLog(@"secondTest");
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
